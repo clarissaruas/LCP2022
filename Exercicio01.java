b52f54e281d6060aa3be9897da547436b0eb1e43
Clarissa Ruas <77933757+clarissaruas@users.noreply.github.com>
2022-05-15 19:16:58 -0300

    Add files via upload

---
+++ Exercicio01.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author cbgru
+ */
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite um número de 1 a 20: ");
+        int num = scan.nextInt();
+        for (int i = 0; i < num; i++){
+            int z;
+            for (z=0; z < i+1; z++){
+                System.out.printf("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
